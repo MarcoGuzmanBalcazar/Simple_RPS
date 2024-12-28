d6e80512cc380ee46ed65fe9f5853195110ce235
MarcoGuzman-Balcazar <maguzm2@emory.edu>
2024-12-28 15:40:50 -0500

    RPS UI Created

--- RPS.js
+++ RPS.js
@@ -28,8 +28,6 @@ function getHumanChoice(){
 }
 
 function playRound(humanChoice, computerChoice) {
-    console.log(humanChoice)
-    console.log(computerChoice)
     if (humanChoice === computerChoice){
         return "Tie"
     }
@@ -43,28 +41,56 @@ function playRound(humanChoice, computerChoice) {
         return "Computer Wins"
     }
   }
-  
-function playgame(){
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
 
-        playRound(humanSelection, computerSelection);
-    }
+function playgame(humanPick){
 
-    console.log(humanScore)
-    console.log(computerScore)
-    if (humanScore > computerScore){
-        return "HUMAN WINS"
-    }
-    else if (computerScore > humanScore){
-        return "COMPUTER WINS"
-    }
-    else{
-        return "TIE"
+    const humanSelection = humanPick;
+    const computerSelection = getComputerChoice();
+
+    playerDecision.textContent = "Human Picked: " + humanPick
+    computerDecision.textContent = "Computer Picked: " + computerSelection
+
+    playRound(humanSelection, computerSelection);
+
+    playerCount.textContent = "Human Score: " + humanScore
+    computerCount.textContent = "Computer Score: " + computerScore
+
+    if (computerScore == 5 || humanScore === 5){
+        if (humanScore > computerScore){
+            winner.textContent = "Human Wins!"
+        }
+        else if (computerScore > humanScore){
+            winner.textContent = "Computer Wins!"
+        }
+        else{
+            winner.textContent = "Its a Tie!"
+        }
     }
 }
 
-console.log(playgame())
+const scissorsButton = document.querySelector("#scissors");
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+
+scissorsButton.addEventListener("click", () => {
+    playgame("Scissors")
+});
+
+rockButton.addEventListener("click", () => {
+    playgame("Rock")
+});
+
+paperButton.addEventListener("click", () => {
+    playgame("Paper")
+});
+
+const playerDecision = document.querySelector("#playerDecision");
+const computerDecision= document.querySelector("#computerDecision");
+
+const playerCount = document.querySelector("#playerCount");
+const computerCount = document.querySelector("#computerCount");
+
+const winner = document.querySelector("#winner")
+
 
 
